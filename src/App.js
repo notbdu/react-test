19c922e7ac6ccb02366d9414df2ddf8a0d50895a
Bo Du <bo@capsule8.com>
2017-09-15 11:35:06 -0400

    Base.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Layer, Rect, Stage, Group} from 'react-konva';
+
+import { FRAMERATE } from './constants';
+
 import './App.css';
+
+class MyRect extends React.Component {
+  state = {
+    color: 'green',
+    x: 0,
+    y: 0,
+  };
+
+  componentDidMount = () => {
+    setInterval(() => this.setState({
+      x: this.state.x+1,
+      y: this.state.y+1,
+    }), FRAMERATE)
+  }
+
+  handleClick = () => {
+    // window.Konva is a global variable for Konva framework namespace
+    this.setState({
+      color: window.Konva.Util.getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <Rect
+        x={this.state.x}
+        y={this.state.y}
+        width={50}
+        height={50}
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={this.handleClick}
+      />
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="score">
+          Scorebox
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="item">
+            <Stage width={700} height={700}>
+              <Layer>
+                <MyRect />
+              </Layer>
+            </Stage>
+          </div>
+        </div>
       </div>
     );
   }
